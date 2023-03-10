2ac294b7661cbedc32c8ef2fbb6ca68968121118
no-decode <127511474+no-decode@users.noreply.github.com>
2023-03-11 00:24:27 +0530

    Update App.js
    Minor change

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Learn React Now
           </a>
         </p>
       </header>
